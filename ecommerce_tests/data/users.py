f0f78ff7ae1d42779b637ccfd584c5e5a3553fa6
viowb <violet.windebank@gmail.com>
2026-03-09 01:55:34 +0000

    POM project 1st commit

---
+++ ecommerce_tests/data/users.py
@@ -0,0 +1,16 @@
+# data/users.py
+
+VALID_USER = {
+    "username": "standard_user",
+    "password": "secret_sauce"
+}
+
+LOCKED_USER = {
+    "username": "locked_out_user",
+    "password": "secret_sauce"
+}
+
+INVALID_USER = {
+    "username": "wrong_user",
+    "password": "wrong_pass"
+}
